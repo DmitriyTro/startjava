903b01aa723bdeaa37220b32c50fbef996893c2f
Dmitrii Trofimof <probcf@gmail.com>
2021-03-10 20:04:53 +0300

    HW_4

--- Lesson_1/ConditionalStatement.java
+++ Lesson_1/ConditionalStatement.java
@@ -1,32 +1,28 @@
 public class ConditionalStatement {
-public static void main(String[] args) {
-    int age = 30;
-    boolean isGenderMale = true;
-    double height = 1.50;
-    char firstLetterName = 'A';
-
-    if (age > 20) {
-        System.out.println("Adult!");
-    }
-
-    if (isGenderMale) {
-        System.out.println("Male!");
-    }
-
-    if (!isGenderMale){
-        System.out.println("Woman!");
-    }
-    if (height < 1.80) {
-        System.out.println("Low!");
-    }else {
-        System.out.println("High!");
-    }
-
+    public static void main(String[] args) {
+        int age = 30;
+        if (age > 20) {
+            System.out.println("Adult!");
+        }
+        boolean isGenderMale = true;
+        if (isGenderMale) {
+            System.out.println("Male!");
+        }
+        if (!isGenderMale) {
+            System.out.println("Woman!");
+        }
+        double height = 1.50;
+        if (height < 1.80) {
+            System.out.println("Low!");
+        } else {
+            System.out.println("High!");
+        }
+        char firstLetterName = 'A';
         if (firstLetterName == 'M') {
             System.out.println("Yes");
-        }else if (firstLetterName == 'I') {
+        } else if (firstLetterName == 'I') {
             System.out.println("No");
-        }else{
+        } else {
             System.out.println("Error!");
         }
     }
--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,12 +1,19 @@
 public class Variable {
     public static void main(String[] arges) {
-        int sum1 = 1;
-        long sum2 = 2l;
-        short sum3 = 3;
-        byte sum4 = 4;
-        float sum5 = 5.1f;
-        double sum6 = 6;
-        char sym = "A";
-        boolean isFalse = true;
+        int cpuCores = 8;
+        long lenght = 60l;
+        short height = 50;
+        byte gpuMemory = 8;
+        float weight = 5.1f;
+        double ram = 16;
+        char sym = 'I';
+        boolean isGoodComputer = true;
+            System.out.println("CPU cores: " + cpuCores);
+            System.out.println("GPU memory: " + gpuMemory);
+            System.out.println("Lenght: " + lenght);
+            System.out.println("Height: " + height);
+            System.out.println("Weight: " + weight);
+            System.out.println("Marker: " + sym);
+            System.out.println("is good? " + isGoodComputer);
     }
 }
