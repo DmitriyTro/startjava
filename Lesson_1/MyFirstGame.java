457a5bb6a9e3f81d48c64dd637ece7270986b468
Dmitrii Trofimof <probcf@gmail.com>
2021-03-11 20:54:42 +0300

    HW_7

---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,23 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int playerNumber = 20;
+        int computerNumber = 30;
+
+        System.out.println("Старт!");
+
+        while(true) {
+            if (playerNumber == computerNumber) {
+                System.out.println("Поздравляю, Вы угадали!");
+                break;
+            } else if (playerNumber > computerNumber) {
+                System.out.println("Вы ввели число: " + playerNumber + " которое больше, чем загадал компьютер!");
+                playerNumber--;
+            } else if (playerNumber < computerNumber) {
+                System.out.println("Вы ввели число: " + playerNumber + " которое меньше, чем загадал компьютер!");
+                playerNumber++;
+            }
+        }
+
+        System.out.println("Вы победили!");
+    }
+}
